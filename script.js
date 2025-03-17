618b3b0a9762780d768f231118bee495a5ec5197
LuisDUF <luis.duran.farias.04@gmail.com>
2025-03-16 21:06:47 -0600

    Filtro pantalla perros

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function cambiar_nav(){
-    
-}
+
+   let radios = document.querySelectorAll("[type='radio']");
+   radios.forEach((x)=>{
+     x.dataset.val = x.checked; // guardamos el estado del radio button dentro del elemento
+     x.addEventListener('click',(e)=>{
+        alert()
+       let element = e.target;
+       if(element.dataset.val == 'false'){
+         element.dataset.val = 'true';
+         element.checked = true;
+       }else{
+         element.dataset.val = 'false';
+         element.checked = false;
+       }
+     },true);
+   });
