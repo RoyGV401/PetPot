1958dd798fd41219427e89fa421890152bfd077f
roy <gomezvalencia.rogelioisaac.proga@gmail.com>
2025-04-29 07:20:03 -0600

    Small change and test on the maps api

--- scripts/maps_api_test.js
+++ scripts/maps_api_test.js
@@ -2,45 +2,57 @@ let latitud = 0;
 let longitud = 0;
 
 async function iniciarMap() {
-    const so = await getUserLocation();
-    const coord = { lat: so[0], lng: so[1] };
-  
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 10,
-      center: coord,
-    });
-  
-    const marker = new google.maps.Marker({
-      position: coord,
-      map: map,
-    });
-  }
-  
-  window.onload = function () {
-    iniciarMap();
-  };
-  
-  function getUserLocation() {
-    return new Promise((resolve, reject) => {
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const latitud = position.coords.latitude;
-            const longitud = position.coords.longitude;
-            resolve([latitud, longitud]);
-          },
-          (error) => {
-            alert('Error al obtener la ubicación.');
-            resolve([0, 0]); // Fallback if denied or failed
-          }
-        );
-      } else {
-        alert('ALERTA: Parece que su navegador no soporta geolocalización.');
-        resolve([0, 0]);
-      }
-    });
-  }
-  
+  const so = await getUserLocation();
+  const coord = { lat: so[0], lng: so[1] };
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const map = new Map(document.getElementById("map"), {
+    center:coord,
+    zoom: 14,
+    mapId: "4504f8b37365c3d0",
+  });
+  const infoWindow = new InfoWindow();
+  const draggableMarker = new AdvancedMarkerElement({
+    map,
+    position: coord,
+    gmpDraggable: true,
+    title: "This marker is draggable.",
+  });
+
+  draggableMarker.addListener("dragend", (event) => {
+    const position = draggableMarker.position;
+
+    infoWindow.close();
+    infoWindow.setContent(`Pin dropped at: ${position.lat}, ${position.lng}`);
+    infoWindow.open(draggableMarker.map, draggableMarker);
+  });
+}
+
+window.onload = function () {
+  iniciarMap();
+};
+
+function getUserLocation() {
+  return new Promise((resolve, reject) => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitud = position.coords.latitude;
+          const longitud = position.coords.longitude;
+          resolve([latitud, longitud]);
+        },
+        (error) => {
+          alert("Error al obtener la ubicación.");
+          resolve([0, 0]); // Fallback if denied or failed
+        }
+      );
+    } else {
+      alert("ALERTA: Parece que su navegador no soporta geolocalización.");
+      resolve([0, 0]);
+    }
+  });
+}
+
 //Esta fución regresa un arreglo de dos valores, el primer corresponde a la latitud, y el segundo a la longitud
 
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
